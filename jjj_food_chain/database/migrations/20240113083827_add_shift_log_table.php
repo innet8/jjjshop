c62e77f498b4d13a06448e28ed1d28bf09e9d693
ganzizi <zhicheng123@126.com>
2024-01-13 18:16:29 +0800

    feat: 新增收银交班

--- jjj_food_chain/database/migrations/20240113083827_add_shift_log_table.php
+++ jjj_food_chain/database/migrations/20240113083827_add_shift_log_table.php
@@ -4,7 +4,7 @@ use think\migration\Migrator;
 use think\migration\db\Column;
 use Phinx\Db\Adapter\MysqlAdapter;
 
-class AddshiftLogTable extends Migrator
+class AddShiftLogTable extends Migrator
 {
     /**
      * Change Method.
