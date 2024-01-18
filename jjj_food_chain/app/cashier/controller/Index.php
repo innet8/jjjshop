01c41650ffde02702e384a5e71a2b0442ca16dfe
ganzizi <zhicheng123@126.com>
2024-01-18 14:51:38 +0800

    feat: 新增收银机货币信息返回

--- jjj_food_chain/app/cashier/controller/Index.php
+++ jjj_food_chain/app/cashier/controller/Index.php
@@ -16,11 +16,28 @@ class Index extends Controller
      * @Apidoc\Title("基础信息")
      * @Apidoc\Method ("POST")
      * @Apidoc\Url ("/index.php/cashier/index/index")
-     * @Apidoc\Returned()
+     * @Apidoc\Returned("currency", type="object", desc="货币", children={
+     *    @Apidoc\Param ("unit",type="string",desc="主单位"),
+     *    @Apidoc\Param ("is_open",type="string",desc="是否开启副单位 0-关闭 1-开启"),
+     *    @Apidoc\Param ("vices",type="object",desc="副单位",children={
+     *       @Apidoc\Param ("vice_unit",type="string",desc="副单位"),
+     *      @Apidoc\Param ("unit_rate",type="float",desc="副单位比例"),
+     *   }),
+     * })
      */
     public function index()
     {
         $user = $this->cashier;
+        // 货币信息
+        $currency = SettingModel::getItem('currency');
+        $user['currency'] = [
+            'unit' => $currency['unit'],
+            'is_open' => $currency['is_open'],
+            'vices' => [
+                'vice_unit' => $currency['vice_unit'],
+                'unit_rate' => $currency['unit_rate'],
+            ],
+        ];
         return $this->renderSuccess('', compact('user'));
     }
 
