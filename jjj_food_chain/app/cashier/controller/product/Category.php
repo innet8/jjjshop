a48b99bc64507ff2becdc8093e8a99ba99a0a5b7
Meng <381618248@qq.com>
2024-01-25 16:30:11 +0800

    feat:收银商品特色分类

--- jjj_food_chain/app/cashier/controller/product/Category.php
+++ jjj_food_chain/app/cashier/controller/product/Category.php
@@ -16,12 +16,13 @@ class Category extends Controller
      * @Apidoc\Title("列表")
      * @Apidoc\Method ("POST")
      * @Apidoc\Url ("/index.php/cashier/product.category/index")
+     * @Apidoc\Param("is_spec", type="int", require=false, default="", desc="是否特色分类 0-否 1-是")
      * @Apidoc\Returned("list",type="array",ref="app\cashier\model\product\Category\getCashierALL")
      */
     public function index()
     {
         $model = new CategoryModel;
-        $list = $model->getCashierALL(1, 0, $this->cashier['user']['shop_supplier_id']);
+        $list = $model->getCashierALL(1, $this->cashier['user']['shop_supplier_id']);
         return $this->renderSuccess('', compact('list'));
     }
 }
