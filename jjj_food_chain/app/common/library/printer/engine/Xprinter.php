69e28e7b0c839f3d5059f6d549b0cd03d871ea63
weifashi <605403358@qq.com>
2024-01-25 16:51:22 +0800

    test:  测试

--- jjj_food_chain/app/common/library/printer/engine/Xprinter.php
+++ jjj_food_chain/app/common/library/printer/engine/Xprinter.php
@@ -19,8 +19,7 @@ class Xprinter extends Basics
      */
     public function printTicket($content)
     {
-        dump($content);
-        die;
+      
         $config = json_decode($this->config, true);
         $content = hex2bin($content);
         $fp=@fsockopen( $config['IP'] ?? self::PRINTER_IP, $config['PORT'] ??  self::PRINTER_PORT,$errno,$errstr, 3);
@@ -32,23 +31,9 @@ class Xprinter extends Basics
         if (!preg_match('/[\p{Thai}]/u', $content)) {
             $content = iconv("UTF-8", "GBK//IGNORE", $content);
         } else {
-            $content = iconv("UTF-8", "UTF-8//IGNORE", $content);
+            $content = iconv("UTF-8", "CP874//IGNORE", $content);
         }
-        
-        // 发送指令和数据
-        fwrite($fp, "\x1B@");  // 初始化打印机
-        fwrite($fp, "\x1BA");  // 选择字符编码
-        fwrite($fp, "\x1BB\x0D");  // 设置字符大小
-        fwrite($fp, "\x1BT\x03");  // 设置字符间距
-
-        // 发送泰语文本
-        $thai_text = "สวัสดีชาวโลก";  // "世界你好"的泰语翻译
-        fwrite($fp, iconv("UTF-8", "CP874", $thai_text));  // 将UTF-8编码转换为泰语编码CP874
-
-        // 发送打印指令
-        fwrite($fp, "\x1BD");  // 切纸
-
-        // fwrite($fp, $content);
+        fwrite($fp, $content);
         //关闭打印机连接
         fclose($fp);
         // 
