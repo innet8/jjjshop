916c29849b24651b573b80689d38382a6d9b35e2
HEXIANG <869411657@qq.com>
2024-01-25 20:38:54 +0800

    fix:bug修复

--- jjj_food_chain_shop/src/views/card/card/part/set.vue
+++ jjj_food_chain_shop/src/views/card/card/part/set.vue
@@ -11,7 +11,7 @@
       <div class="gray9">{{ $t('有效期限（月），0为永久有效') }}</div>
     </el-form-item>
     <el-form-item :label="$t('价格：')" :rules="[{ required: true, message:  $t('请填写价格') }]" prop="model.money">
-      <el-input-number :controls="false" :min="0"  class="max-w460" :placeholder="$t('请填写有效期')" v-model.number="form.model.money"></el-input-number>
+      <el-input-number :controls="false" :min="0"  class="max-w460" :placeholder="$t('请填写价格')" v-model.number="form.model.money"></el-input-number>
       <div class="gray9">{{ $t('用户购买会员卡需要支付的金额，0为用户免费领取') }}</div>
     </el-form-item>
 <!--    <el-form-item label="库存：" :rules="[{ required: true, message: '请填写库存' }]" prop="model.stock">
