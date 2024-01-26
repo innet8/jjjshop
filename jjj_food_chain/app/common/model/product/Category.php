3f3087e593997614a0d08bc0d363a9df1702b791
Meng <381618248@qq.com>
2024-01-26 10:12:17 +0800

    perf:商品特色分类排序

--- jjj_food_chain/app/common/model/product/Category.php
+++ jjj_food_chain/app/common/model/product/Category.php
@@ -176,7 +176,7 @@ class Category extends BaseModel
             ->where('parent_id', '=', 0)
             ->where('type', '=', $type)
             ->where('status', '=', 1)
-            ->order(['sort' => 'asc', 'create_time' => 'asc'])
+            ->order(['is_special' => 'desc', 'sort' => 'asc', 'create_time' => 'asc'])
             ->where('shop_supplier_id', '=', $shop_supplier_id)
             ->select();
         // 
