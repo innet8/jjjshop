ffb51f134e17e9481ea22543d0d142f4056ad286
ganzizi <zhicheng123@126.com>
2024-01-26 10:10:00 +0800

    fix: doc

--- jjj_food_chain/config/apidoc.php
+++ jjj_food_chain/config/apidoc.php
@@ -108,7 +108,7 @@ return [
             ['name'=>'Token','type'=>'string','require'=>true,'desc'=>'身份令牌Token（平板、厨显不需要）'],
             ['name'=>'Appid','type'=>'string','require'=>true, 'desc'=>'应用Appid - app_id'],
             ['name'=>'Sid','type'=>'string','require'=>true, 'desc'=>'门店Sid - shop_supplier_id'],
-            ['name'=>'Tid','type'=>'string','require'=>true, 'desc'=>'桌台ID(平板专用) - table_id'],
+            ['name'=>'Tid','type'=>'string','require'=>false, 'desc'=>'桌台ID(平板专用) - table_id'],
         ],
         // （选配）全局的请求Query
         'query'=>[
