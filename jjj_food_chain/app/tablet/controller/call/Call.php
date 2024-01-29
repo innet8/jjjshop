6eb6ba90618e491f4ef12be9e34d01656a7a251a
Meng <381618248@qq.com>
2024-01-29 14:46:26 +0800

    perf:平板呼叫服务员

--- jjj_food_chain/app/tablet/controller/call/Call.php
+++ jjj_food_chain/app/tablet/controller/call/Call.php
@@ -23,7 +23,7 @@ class Call extends Controller
      */
     public function call($table_id, $call_type, $shop_supplier_id)
     {
-        $tableNo = Table::where('table_id', $table_id)->where('status', 30)->value('table_no');
+        $tableNo = Table::where('table_id', $table_id)->value('table_no');
         if (!$tableNo) {
             return $this->renderError('桌台已关闭');
         }
