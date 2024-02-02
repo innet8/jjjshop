b5e789ce981980feacec48cb9f1312451b19356c
weifashi <605403358@qq.com>
2024-02-02 19:07:12 +0800

    feat:  送厨 - 商品库存不足时 返回详细信息

--- jjj_food_chain/app/cashier/controller/order/Order.php
+++ jjj_food_chain/app/cashier/controller/order/Order.php
@@ -273,7 +273,7 @@ class Order extends Controller
             TableModel::close($detail['table_id']);
             return $this->renderSuccess('结账成功');
         }
-        return $this->renderError($detail->getError() ?: '结账失败');
+        return $this->renderError($detail->getError() ?: '结账失败', $detail->getErrorData());
     }
 
     /**
