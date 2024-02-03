5a9e34bf353043077cf56cdf4bf937c6148569d1
weifashi <605403358@qq.com>
2024-02-03 11:38:11 +0800

    build: 编译资源上传

---
+++ jjj_food_chain/public/shop/static/js/fullreduce-d01d1882.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-e6591672.js";let s={getList:(s,l)=>e._post("/shop/plus.fullreduce/index",s,l),add:(s,l)=>e._post("/shop/plus.fullreduce/add",s,l),edit:(s,l)=>e._post("/shop/plus.fullreduce/edit",s,l),del:(s,l)=>e._post("/shop/plus.fullreduce/delete",s,l)};export{s as F};
