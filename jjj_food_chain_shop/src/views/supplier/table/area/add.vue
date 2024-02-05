cb06e43ba3e7649f27e08d0f7a1e60d7d293fe50
HEXIANG <869411657@qq.com>
2024-02-05 13:47:31 +0800

    fix:添加修改

--- jjj_food_chain_shop/src/views/supplier/table/area/add.vue
+++ jjj_food_chain_shop/src/views/supplier/table/area/add.vue
@@ -66,7 +66,7 @@ export default {
             self.$refs.form.validate((valid) => {
                 if (valid) {
                     self.loading = true;
-                    StoreApi.addArea(params).then(data => {
+                    StoreApi.addArea(params, true).then(data => {
                         self.loading = false;
                         this.$ElMessage({
                             message: $t('添加成功'),
