5caea245cf3a8f58a04d1e835a93b52f39112eae
weifashi <605403358@qq.com>
2024-02-07 18:48:54 +0800

    build: 编译资源上传

--- jjj_food_chain/public/shop/static/js/index-legacy-240684d0.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-fffc2d0f.js"],(function(e,t){"use strict";var n;return{setters:[function(e){n=e.r}],execute:function(){e("I",{base:function(e,t){return n._post("/shop/index/base",e,t)},getCount:function(e,t){return n._post("/shop/Index/index",e,t)}})}}}));
