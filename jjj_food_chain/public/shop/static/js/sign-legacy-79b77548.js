5b01cb5b3c56336c2a03531275e7cd4605ca238e
ganzizi <zhicheng123@126.com>
2024-03-01 16:45:34 +0800

    build

--- jjj_food_chain/public/shop/static/js/sign-legacy-79b77548.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-a74d7db4.js"],(function(t,n){"use strict";var s;return{setters:[function(t){s=t.r}],execute:function(){t("S",{getData:function(t,n){return s._get("/shop/plus.sign/index",t,n)},saveData:function(t,n){return s._post("/shop/plus.sign/index",t,n)},getSignList:function(t,n){return s._post("/shop/plus.sign/lists",t,n)}})}}}));
