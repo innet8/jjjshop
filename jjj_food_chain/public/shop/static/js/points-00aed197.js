dfa6ab0736fb566f12414b1985ffd2538a67c1f9
ganzizi <zhicheng123@126.com>
2024-03-05 15:01:35 +0800

    build

--- jjj_food_chain/public/shop/static/js/points-00aed197.js
+++
@@ -1,1 +0,0 @@
-import{r as s}from"./index-417beca7.js";let t={setPoints:(t,e)=>s._post("/shop/user.points/setting",t,e),getPoints:(t,e)=>s._get("/shop/user.points/setting",t,e),GetUserList:(t,e)=>s._get("/shop/user.points/log",t,e)};export{t as P};
