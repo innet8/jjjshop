2e6377f8414c7a2e18eb2384a9b89225c5ed1c9c
ganzizi <zhicheng123@126.com>
2024-03-05 16:51:28 +0800

    build

---
+++ jjj_food_chain/public/shop/static/js/sign-e2ceec07.js
@@ -0,0 +1,1 @@
+import{r as s}from"./index-e8d31a3a.js";let t={getData:(t,i)=>s._get("/shop/plus.sign/index",t,i),saveData:(t,i)=>s._post("/shop/plus.sign/index",t,i),getSignList:(t,i)=>s._post("/shop/plus.sign/lists",t,i)};export{t as S};
