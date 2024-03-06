ac76c39d78be5a9f2691b451111bb1c26fd61704
ganzizi <zhicheng123@126.com>
2024-03-06 16:07:45 +0800

    build

--- jjj_food_chain/public/shop/static/js/fullreduce-b7fb5ec2.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-234d26a3.js";let s={getList:(s,l)=>e._post("/shop/plus.fullreduce/index",s,l),add:(s,l)=>e._post("/shop/plus.fullreduce/add",s,l),edit:(s,l)=>e._post("/shop/plus.fullreduce/edit",s,l),del:(s,l)=>e._post("/shop/plus.fullreduce/delete",s,l)};export{s as F};
