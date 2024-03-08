a8d550e239e9f01fc2d1f35c3a635bb357e5611a
weifs <605403358@qq.com>
2024-03-08 14:10:07 +0800

    feat:  关闭数据库语句监听

--- jjj_food_chain/config/database.php
+++ jjj_food_chain/config/database.php
@@ -51,7 +51,7 @@ return [
             // 是否需要断线重连
             'break_reconnect' => false,
             // 监听SQL
-            'trigger_sql' => false,
+            'trigger_sql' => Env::get('APP_DEBUG', true) ,
             // 开启字段缓存
             'fields_cache' => false,
             // 字段缓存路径
