3309f7d490a98feab32e7c0e3d02acdd9ddafea5
Meng <381618248@qq.com>
2024-03-13 12:12:07 +0800

    feat: shop_opt_log字段content类型修改迁表文件

---
+++ jjj_food_chain/database/migrations/20240313115402_update_content_to_shop_opt_log_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use think\migration\Migrator;
+use think\migration\db\Column;
+
+class UpdateContentToShopOptLogTable extends Migrator
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table = $this->table('shop_opt_log');
+        $table->changeColumn('content', Column::longText('content')->setComment('操作内容'));
+        $table->update();
+    }
+}
