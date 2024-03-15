d8b9d5b92401cc40067dbfb9c8b3a358de997bf4
weifs <605403358@qq.com>
2024-03-15 15:06:18 +0800

    feat: 调整文本

--- jjj_food_chain_shop/src/views/supplier/printing/printing/index.vue
+++ jjj_food_chain_shop/src/views/supplier/printing/printing/index.vue
@@ -52,12 +52,12 @@
             </el-form-item>
             <el-form-item :label="$t('选择打印机')" v-if="form.cashier_open == 1" class="cashier-item">
                 <el-select class="max-w460" v-model="form.cashier_printer_id" :placeholder="$t('请选择')">
-                    <el-option :label="'sunmi ' + $t('收银打印机 自带')" :value="'0'">
-                        <span style="float: left">sunmi {{ $t('收银打印机') }}</span>
+                    <el-option :label="'sunmi 商米 ' + $t('收银打印机 自带')" :value="'0'">
+                        <span style="float: left">sunmi 商米 {{ $t('收银打印机') }}</span>
                         <span style="float: right; color: #8492a6; font-size: 13px">{{ $t('自带') }}</span>
                     </el-option>
-                    <el-option :label="'UroVo优博讯 ' + $t('收银打印机 自带')" :value="'-1'">
-                        <span style="float: left">UroVo优博讯 {{ $t('收银打印机') }}</span>
+                    <el-option :label="'UroVo 优博讯 ' + $t('收银打印机 自带')" :value="'-1'">
+                        <span style="float: left">UroVo 优博讯 {{ $t('收银打印机') }}</span>
                         <span style="float: right; color: #8492a6; font-size: 13px">{{ $t('自带') }}</span>
                     </el-option>
                     <el-option v-for="(item, index) in printerList" :key="index" :label="item.printer_name"
